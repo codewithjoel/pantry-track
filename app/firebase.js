f0c51f1f6375414bc7cd100fd7821b053ea1d30c
codewithjoel <157177859+codewithjoel@users.noreply.github.com>
2024-08-08 10:04:19 -0500

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -7,12 +7,12 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD1bXYunotg5jysEh806qmP_CfT4TPfVNo",
+  apiKey: "API_KEY",
   authDomain: "pantry-tracker-c2ece.firebaseapp.com",
   projectId: "pantry-tracker-c2ece",
   storageBucket: "pantry-tracker-c2ece.appspot.com",
   messagingSenderId: "489862803454",
-  appId: "1:489862803454:web:11a032138425c3aae21aaf"
+  appId: "API_ID"
 };
 
 // Initialize Firebase
